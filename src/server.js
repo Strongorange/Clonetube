1f25559ee15408f3cea938773ef1dc51c0bf1921
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-04-27 00:11:43 +0900

    042721

--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
-import "./db";
 import express from "express"; //express 라는 패키지를 "express" 애서 가져옴 nodeJs가 node modules에서 알아서 찾아줌 똑똑이
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -18,7 +15,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-const handleListening = () =>
-  console.log(`server listening on port http://localhost:${PORT} 💕`);
-
-app.listen(PORT, handleListening);
+export default app;
