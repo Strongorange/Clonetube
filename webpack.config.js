ee267e1565134a0b55966a9cceee6e34d1dac41e
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-05-12 16:31:15 +0900

    Done Webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+//옛날 자바스크립트 코드만 이해함
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/client/js/main.js",
+  mode: "development",
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"), //webpack 이 main 파일 주물러서 assets에 저장
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], //webpack 은 뒤에서부터 시작 시작순서대로 놓아야함
+      },
+    ],
+  },
+};
